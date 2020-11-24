ba652435b98a0304d35c8c4ddb02450528d476a3
HyeYeonCha <gpdus335@naver.com>
2020-11-24 22:03:47 +0900

    edit quickSort

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -1,32 +1,32 @@
 const quickSort = function (arr, callback) {
-    // TODO: 여기에 코드를 작성합니다.
-    if(arr.length <= 1)
-      return arr;
-  
-    let pivot = arr[0];
-    let left = [];
-    let right = [];
-  
-    if(callback){
-      for(let i=1; i<arr.length; i++){
-        if(callback(pivot) < callback(arr[i])){
-          right.push(arr[i]);
-         }else {
-          left.push(arr[i]);
-       }
-     }
-    }else{
-      for(let i=1; i<arr.length; i++){
-        if(pivot < arr[i]){
-          right.push(arr[i]);
-        }else {
-          left.push(arr[i]);
-       }
-     }
+  // TODO: 여기에 코드를 작성합니다.
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
+
+  if (callback) {
+    for (let i = 1; i < arr.length; i++) {
+      if (callback(pivot) < callback(arr[i])) {
+        right.push(arr[i]);
+      } else {
+        left.push(arr[i]);
+      }
     }
-    let leftArr = quickSort(left);
-    let rightArr = quickSort(right);
-   
-    return [...leftArr, pivot, ...rightArr]; 
-  };
-  
+  } else {
+    for (let i = 1; i < arr.length; i++) {
+      if (pivot < arr[i]) {
+        right.push(arr[i]);
+      } else {
+        left.push(arr[i]);
+      }
+    }
+  }
+  let leftArr = quickSort(left);
+  let rightArr = quickSort(right);
+
+  return [...leftArr, pivot, ...rightArr];
+};
