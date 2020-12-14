072d550440ea393c04d0d818a0c6a1f73eedd497
HyeYeonCha <gpdus335@naver.com>
2020-12-14 22:42:41 +0900

    edit robotPath2

--- robotPath2/robotPath2.js
+++ robotPath2/robotPath2.js
@@ -1,94 +1,157 @@
 const robotPath2 = function (room, src, sDir, dst, dDir) {
-    // 가로와 세로의 길이
-    const R = room.length;
-    const C = room[0].length;
-    // 4가지 방향: 위(1), 오른쪽(2), 아래(3), 왼쪽(4)
-    // 차례대로 [방향, 상하이동, 좌우이동]
-    const MOVES = [
+  // 가로와 세로의 길이
+  const R = room.length;
+  const C = room[0].length;
+  // 4가지 방향: 위(1), 오른쪽(2), 아래(3), 왼쪽(4)
+  // 차례대로 [방향, 상하이동, 좌우이동]
+  const MOVES = [
+    [1, -1, 0], // UP
+    [2, 0, 1], // RIGHT
+    [3, 1, 0], // DOWN
+    [4, 0, -1], // LEFT
+  ];
+
+  // 좌표가 유효한 좌표인지 확인하는 함수
+  const isValid = (row, col) => row >= 0 && row < R && col >= 0 && col < C;
+
+  // 각 위치별 최소의 동작으로 도달 가능한 경우의 방향을 저장
+  const directions = [];
+  // 각 위치별 최소 동작의 수를 저장. 편의상 거리(dist)로 표현
+  const dist = [];
+  for (let row = 0; row < R; row++) {
+    directions.push(Array(C).fill(0));
+    dist.push(Array(C).fill(Number.MAX_SAFE_INTEGER));
+  }
+
+  // bfs 구현을 위해 큐를 선언한다.
+  const queue = Array(R * C);
+  let front = 0;
+  let rear = 0;
+  const isEmpty = (queue) => front === rear;
+  const enQueue = (queue, pos) => {
+    queue[rear] = pos;
+    rear++;
+  };
+  const deQueue = (queue) => {
+    const pos = queue[front];
+    front++;
+    return pos;
+  };
+
+  // 출발 지점의 좌표
+  const [sRow, sCol] = src;
+  directions[sRow][sCol] = sDir;
+  dist[sRow][sCol] = 0;
+
+  // 목표 지점의 좌표
+  const [dRow, dCol] = dst;
+
+  enQueue(queue, [sRow, sCol]);
+  while (isEmpty(queue) === false) {
+    const [row, col] = deQueue(queue);
+    const dir = directions[row][col];
+
+    for (move of MOVES) {
+      const [nDir, rDiff, cDiff] = move;
+      // 이동할 좌표
+      const nRow = row + rDiff;
+      const nCol = col + cDiff;
+
+      // 유효한 좌표가 아니거나
+      // 해당 좌표가 장애물(1)인 경우 건너뛴다.
+      if (isValid(nRow, nCol) === false || room[nRow][nCol] === 1) continue;
+
+      // 현재 위치의 방향과 목표 위치의 방향과의 차이
+      const dDiff = Math.abs(nDir - dir);
+      let candidate;
+      if (dDiff === 0) {
+        // 차이가 없는 경우
+        // 출발 지점에서의 방향과 이동하려는 방향이 같은 경우
+        // 직진만 하면 되지만 그러기 위해서는 1로 초기화 되어야 한다.
+        candidate = dist[row][col] || 1;
+      } else if (dDiff === 2) {
+        // 2번 회전해야 하는 경우: 회전 2 + 직진 1
+        candidate = dist[row][col] + 3;
+      } else {
+        // 1번만 회전해도 되는 경우: 회전 1 + 직진 1
+        candidate = dist[row][col] + 2;
+      }
+
+      if (candidate < dist[nRow][nCol]) {
+        // 유망한 좌표는 큐에 삽입한다.
+        enQueue(queue, [nRow, nCol]);
+        dist[nRow][nCol] = candidate;
+        // 방향은 전부 같다.
+        directions[nRow][nCol] = nDir;
+      }
+
+      if (nRow === dRow && nCol === dCol) {
+        // 목표 지점에 도착한 경우
+        // 목표 방향과 맞는지 확인한 후
+        // 최종 결과 값에서 1을 뺀다.
+        const dDiff = Math.abs(nDir - dDir);
+        if (dDiff === 0) {
+          return dist[dRow][dCol];
+        } else if (dDiff === 2) {
+          return dist[dRow][dCol] + 2;
+        } else {
+          return dist[dRow][dCol] + 1;
+        }
+      }
+    }
+  }
+};
+
+/*
+const robotPath2 = function (room, src, sDir, dst, dDir) {
+  // TODO: 여기에 코드를 작성합니다.
+  let col = room[0].length;
+  let row = room.length;
+  const MOVES = [
       [1, -1, 0], // UP
       [2, 0, 1], // RIGHT
       [3, 1, 0], // DOWN
       [4, 0, -1], // LEFT
     ];
-  
-    // 좌표가 유효한 좌표인지 확인하는 함수
-    const isValid = (row, col) => row >= 0 && row < R && col >= 0 && col < C;
-  
-    // 각 위치별 최소의 동작으로 도달 가능한 경우의 방향을 저장
-    const directions = [];
-    // 각 위치별 최소 동작의 수를 저장. 편의상 거리(dist)로 표현
-    const dist = [];
-    for (let row = 0; row < R; row++) {
-      directions.push(Array(C).fill(0));
-      dist.push(Array(C).fill(Number.MAX_SAFE_INTEGER));
-    }
-  
-    // bfs 구현을 위해 큐를 선언한다.
-    const queue = Array(R * C);
-    let front = 0;
-    let rear = 0;
-    const isEmpty = (queue) => front === rear;
-    const enQueue = (queue, pos) => {
-      queue[rear] = pos;
-      rear++;
-    };
-    const deQueue = (queue) => {
-      const pos = queue[front];
-      front++;
-      return pos;
-    };
-  
-    // 출발 지점의 좌표
-    const [sRow, sCol] = src;
-    directions[sRow][sCol] = sDir;
-    dist[sRow][sCol] = 0;
-  
-    // 목표 지점의 좌표
-    const [dRow, dCol] = dst;
-  
-    enQueue(queue, [sRow, sCol]);
-    while (isEmpty(queue) === false) {
-      const [row, col] = deQueue(queue);
-      const dir = directions[row][col];
-  
-      for (move of MOVES) {
-        const [nDir, rDiff, cDiff] = move;
-        // 이동할 좌표
-        const nRow = row + rDiff;
-        const nCol = col + cDiff;
-  
-        // 유효한 좌표가 아니거나
-        // 해당 좌표가 장애물(1)인 경우 건너뛴다.
-        if (isValid(nRow, nCol) === false || room[nRow][nCol] === 1) continue;
-  
-        // 현재 위치의 방향과 목표 위치의 방향과의 차이
+    
+  let queue = [];
+  let directions = Array.from(Array(row), ()=>Array(col).fill(0));
+  let dist = Array.from(Array(row), ()=>Array(col).fill(Number.MAX_SAFE_INTEGER));
+  //  위쪽이 1, 오른쪽이 2, 아래쪽이 3, 왼쪽이 4
+  let [sRow, sCol] = src;
+
+  queue.push(src);
+  directions[sRow][sCol] = sDir;
+  dist[sRow][sCol] = 0;
+
+  const[dRow, dCol] = dst;
+
+  while(queue.length > 0){
+    const [curRow, curCol] = queue.shift();
+    const dir = directions[curRow][curCol];
+    for(move of MOVES){
+      const [nDir, rDiff, cDiff] = move;
+      let newCol = curCol + cDiff;
+      let newRow = curRow + rDiff;
+
+      if(newRow >=0 && newCol < col && newCol >= 0 && newRow < row && room[newRow][newCol] === 0){
         const dDiff = Math.abs(nDir - dir);
         let candidate;
         if (dDiff === 0) {
-          // 차이가 없는 경우
-          // 출발 지점에서의 방향과 이동하려는 방향이 같은 경우
-          // 직진만 하면 되지만 그러기 위해서는 1로 초기화 되어야 한다.
-          candidate = dist[row][col] || 1;
+          candidate = dist[curRow][curCol] || 1;
         } else if (dDiff === 2) {
-          // 2번 회전해야 하는 경우: 회전 2 + 직진 1
-          candidate = dist[row][col] + 3;
+          candidate = dist[curRow][curCol] + 3;
         } else {
-          // 1번만 회전해도 되는 경우: 회전 1 + 직진 1
-          candidate = dist[row][col] + 2;
+          candidate = dist[curRow][curCol] + 2;
         }
   
-        if (candidate < dist[nRow][nCol]) {
-          // 유망한 좌표는 큐에 삽입한다.
-          enQueue(queue, [nRow, nCol]);
-          dist[nRow][nCol] = candidate;
-          // 방향은 전부 같다.
-          directions[nRow][nCol] = nDir;
+        if (candidate < dist[newRow][newCol]) {
+          queue.push([newRow, newCol]);
+          dist[newRow][newCol] = candidate;
+          directions[newRow][newCol] = nDir;
         }
   
-        if (nRow === dRow && nCol === dCol) {
-          // 목표 지점에 도착한 경우
-          // 목표 방향과 맞는지 확인한 후
-          // 최종 결과 값에서 1을 뺀다.
+        if (newRow === dRow && newCol === dCol) {
           const dDiff = Math.abs(nDir - dDir);
           if (dDiff === 0) {
             return dist[dRow][dCol];
@@ -100,4 +163,8 @@ const robotPath2 = function (room, src, sDir, dst, dDir) {
         }
       }
     }
-  };
+  }
+
+};
+
+  */
