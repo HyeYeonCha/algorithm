40551f6bdc4254bb070a46a8ba7781702cbe91c7
HyeYeonCha <gpdus335@naver.com>
2020-12-28 12:35:18 +0900

    Add coinChange

--- closestPairOfPoints/closestPairOfPoints.js
+++ closestPairOfPoints/closestPairOfPoints.js
@@ -1,27 +1,27 @@
 // 좌표평면 위의 두 점 사이의 거리를 계산하는 함수입니다.
 function calculateDistance(p1, p2) {
-    const yDiff = p2[0] - p1[0];
-    const xDiff = p2[1] - p1[1];
-    let dist = Math.sqrt(Math.pow(yDiff, 2) + Math.pow(xDiff, 2));
-    return Math.round(dist * 100);
-  }
-  
-  const closestPairOfPoints = function (points) {
-    // TODO: 여기에 코드를 작성합니다.
-    let min = Number.MAX_SAFE_INTEGER;
-    let p1, p2;
-    for(let i=0; i<points.length; i++){
-      p1 = points[i];
-      for(let j=i+1; j< points.length; j++){
-        p2 = points[j];
-        min = Math.min(calculateDistance(p1, p2), min);
-      }
-    }
-    return min;
-  };
+  const yDiff = p2[0] - p1[0];
+  const xDiff = p2[1] - p1[1];
+  let dist = Math.sqrt(Math.pow(yDiff, 2) + Math.pow(xDiff, 2));
+  return Math.round(dist * 100);
+}
 
-  // Advanced
-  /*
+const closestPairOfPoints = function (points) {
+  // TODO: 여기에 코드를 작성합니다.
+  let min = Number.MAX_SAFE_INTEGER;
+  let p1, p2;
+  for (let i = 0; i < points.length; i++) {
+    p1 = points[i];
+    for (let j = i + 1; j < points.length; j++) {
+      p2 = points[j];
+      min = Math.min(calculateDistance(p1, p2), min);
+    }
+  }
+  return min;
+};
+
+// Advanced
+/*
 // 좌표평면 위의 두 점 사이의 거리를 계산하는 함수입니다.
 function calculateDistance(p1, p2) {
   const yDiffSquared = Math.pow(p2[0] - p1[0], 2);
